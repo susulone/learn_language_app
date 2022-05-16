6e6ac67199335416bec74cb71e9c516bc9d99a6f
Suvi Sulonen <suvi.sulonen@gmail.com>
2022-05-16 17:41:04 +0300

    refactor(load indicator): redesign the loading indicator

--- client/src/components/LoadIndicator.js
+++ client/src/components/LoadIndicator.js
@@ -1,20 +1,62 @@
 import React, { useContext } from 'react';
 import StylesContext from '../contexts/Styles';
-import { Box, Spinner } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Stack,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+  Divider,
+} from '@chakra-ui/react';
 
 const LoadIndicator = () => {
   const { responsiveContainerSm } = useContext(StylesContext);
 
   return (
-    <Box maxWidth={responsiveContainerSm} align="center" pt="4">
-      <Spinner
-        size="xl"
-        thickness="4px"
-        speed="1s"
-        emptyColor="gray.200"
-        color="blue.500"
-      />
-    </Box>
+    <Stack
+      width={responsiveContainerSm}
+      height="300px"
+      align="center"
+      justify="center"
+      pt="4"
+    >
+      <Box
+        width={responsiveContainerSm}
+        borderWidth="1px"
+        borderRadius="lg"
+        p="5"
+        overflow="hidden"
+        shadow="md"
+      >
+        <Flex
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          pb="3"
+        >
+          <Stack width="75%">
+            <Skeleton height="20px" />
+            <Stack width="80%">
+              <Skeleton height="20px" />
+            </Stack>
+          </Stack>
+          <SkeletonCircle size="10" />
+        </Flex>
+        <Flex direction="column" gap="5">
+          <Divider />
+          <SkeletonText noOfLines={5} spacing="4" />
+          <Stack direction="row" mt="4">
+            <Stack width="20%">
+              <Skeleton height="20px" />
+            </Stack>
+            <Stack width="20%">
+              <Skeleton height="20px" />
+            </Stack>
+          </Stack>
+        </Flex>
+      </Box>
+    </Stack>
   );
 };
 
